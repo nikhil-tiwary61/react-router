e2aafe81aa914c8e8edc8f8004ed68a33a74dd92
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-26 12:55:39 +0530

    handle bad url

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,14 +2,23 @@ import { useEffect, useState } from "react";
 
 export default function Image() {
   const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
       .then((response) => setImageUrl(response[0].url))
-      .catch((error) => console.error(error));
+      .catch((error) => setError(error));
   }, []);
 
+  if (error) {
+    return <p>An error was encountered.</p>;
+  }
   return (
     imageUrl && (
       <>
